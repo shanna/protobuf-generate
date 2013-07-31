b9ee1c050799dce3db616b0dbd37050e3326ee3d
Shane Hanna <shane.hanna@gmail.com>
2013-07-31 14:10:48 +1000

    Wip wip it good.

--- lib/protobuf/generate/parser.rb
+++ lib/protobuf/generate/parser.rb
@@ -3,15 +3,16 @@ require 'parslet'
 module Protobuf
   module Generate
     class Parser < Parslet::Parser
-      rule(:newline)     { match('\n').repeat(1) }
+      rule(:newline)     { match('\n') }
       rule(:whitespace)  { match('\s').repeat(1) }
       rule(:whitespace?) { whitespace.maybe }
       rule(:space)       { match('[\t ]').repeat(1) }
       rule(:space?)      { space.maybe }
 
       rule(:comment_option)      { str('@') >> identifier.as(:name) >> space? >> equals >> constant.as(:value) }
-      rule(:comment_option_list) { (comment_option >> space?).repeat(1).as(:options) }
-      rule(:comment)             { (str('//') >> (newline.absent? >> (comment_option_list | any)).repeat).as(:comment) >> whitespace? }
+      rule(:comment_option_list) { (comment_option >> space?).repeat(1) }
+      rule(:comment)             { str('//') >> (newline.absent? >> (comment_option_list.as(:meta) | any)).repeat >> newline }
+      rule(:comment_list)        { comment.repeat }
 
       rule(:equals)        { str('=') >> whitespace? }
       rule(:bracket_open)  { str('{') >> whitespace? }
@@ -35,9 +36,9 @@ module Protobuf
       rule(:field_type)        { identifier.as(:type) >> whitespace? }
       rule(:field_label)       { (str('required') | str('optional') | str('repeated')).as(:label) >> whitespace? }
 
-      rule(:message_field)       { field_label >> field_type >> identifier.as(:name) >> whitespace? >> equals >> integer.as(:tag) >> whitespace? >> field_option_list.maybe >> str(';') >> space? >> comment.maybe }
+      rule(:message_field)       { field_label >> field_type >> identifier.as(:name) >> whitespace? >> equals >> integer.as(:tag) >> whitespace? >> field_option_list.maybe >> str(';') >> space? >> comment.maybe.as(:comment) }
       rule(:message_field_list)  { (message_field >> whitespace?).repeat(1).as(:fields) }
-      rule(:message)             { (str('message') >> whitespace? >> identifier.as(:name) >> whitespace? >> bracket_open >> message_field_list >> bracket_close >> whitespace?).as(:message) }
+      rule(:message)             { (comment_list.maybe.as(:comments) >> str('message') >> whitespace? >> identifier.as(:name) >> whitespace? >> bracket_open >> message_field_list.maybe >> bracket_close >> whitespace?).as(:message) }
 
       rule(:enum_field)      { identifier.as(:name) >> whitespace? >> equals >> integer.as(:tag) >> whitespace? >> str(';') }
       rule(:enum_field_list) { (enum_field >> whitespace?).repeat(1).as(:fields) }
@@ -45,9 +46,20 @@ module Protobuf
 
       rule(:package) { (str('package') >> whitespace? >> identifier_dot_list.as(:name) >> whitespace? >> str(';') >> whitespace?).as(:package) }
 
-      rule(:expression) { whitespace? >> (comment | package | enum | message).repeat }
+      rule(:expression) { whitespace? >> (package | enum | message | comment | whitespace).repeat }
       root(:expression)
     end # Parser
+
+    class Transform < Parslet::Transform
+      rule(comments: sequence(:c)) do
+        puts "MATCH"
+        true
+        #comments.inject({}) do |acc, comment|
+        #  comment[:meta].each{|m| acc[m[:name]] = m[:value]}
+        #  acc
+        #end
+      end
+    end
   end # Generate
 end # Protobuf
 
